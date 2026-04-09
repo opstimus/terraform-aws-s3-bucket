73617906a22c2a96052d2998b20e07b5358565a6
Umar Arafath <umararafath@opstimus.com>
2026-04-09 09:55:12 +0530

    DEVOPS-424: Add-account-regional-namespace

--- variables.tf
+++ variables.tf
@@ -48,3 +48,14 @@ variable "restrict_public_buckets" {
   type    = bool
   default = true
 }
+
+variable "bucket_namespace" {
+  type        = string
+  description = "Bucket namespace mode. Valid values are account-regional or global."
+  default     = "global"
+
+  validation {
+    condition     = contains(["global", "account-regional"], var.bucket_namespace)
+    error_message = "bucket_namespace must be either \"global\" or \"account-regional\"."
+  }
+}
